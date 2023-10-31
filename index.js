966db9427a6641ed2de89de09c5c99c063b78419
spuqe <CryT0r@protonmail.com>
2023-10-31 12:41:19 +0000

    Fixed bugs

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const readline = require('readline');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 // data
-const etunimet=["Mikko","Anna","Juhani","Liisa","Pekka","Sari","Timo","Elina","Ville","Maria","Heikki","Sofia","Juha","Laura","Eero","Hanna","Jussi","Aino","Janne","Kaisa","Teemu","Satu","Antti","Katja","Matti","Niina","Jari","Anni","Mika","Leena","Tuomas","Ella","Risto","Elisa","Samuli","Helena","Tuukka","Mari","Lauri","Marika","Aki","Riikka","Veikko","Kati","Sami","Henna","Seppo","Susanna"],
+const etunimet = ["Mikko", "Anna", "Juhani", "Liisa", "Pekka", "Sari", "Timo", "Elina", "Ville", "Maria", "Heikki", "Sofia", "Juha", "Laura", "Eero", "Hanna", "Jussi", "Aino", "Janne", "Kaisa", "Teemu", "Satu", "Antti", "Katja", "Matti", "Niina", "Jari", "Anni", "Mika", "Leena", "Tuomas", "Ella", "Risto", "Elisa", "Samuli", "Helena", "Tuukka", "Mari", "Lauri", "Marika", "Aki", "Riikka", "Veikko", "Kati", "Sami", "Henna", "Seppo", "Susanna"],
 
-sukunimet=["Korhonen","Virtanen","M\xe4kinen","Nieminen","Koskinen","H\xe4m\xe4l\xe4inen","Laine","Lehtonen","Heikkinen","J\xe4rvinen","Laaksonen","Miettinen","Koivisto","M\xe4kel\xe4","Rantanen","Salminen","Kinnunen","Jokinen","Aaltonen","Hakala","Tuominen","Lindroos","Karppinen","Laitinen","Kujala","Korpela","Lindstr\xf6m","Mikkola","Laukkanen","Tolonen","Savolainen","Pakarinen","Kokko","Virta","Karjalainen","Koivula","Koivu","Laukka","Saari","Kallio","Hiltunen","Rinne","Rissanen","Hirvonen","Ranta","Korpi","Koskela","Kukkonen"],
+  sukunimet = ["Korhonen", "Virtanen", "M\xe4kinen", "Nieminen", "Koskinen", "H\xe4m\xe4l\xe4inen", "Laine", "Lehtonen", "Heikkinen", "J\xe4rvinen", "Laaksonen", "Miettinen", "Koivisto", "M\xe4kel\xe4", "Rantanen", "Salminen", "Kinnunen", "Jokinen", "Aaltonen", "Hakala", "Tuominen", "Lindroos", "Karppinen", "Laitinen", "Kujala", "Korpela", "Lindstr\xf6m", "Mikkola", "Laukkanen", "Tolonen", "Savolainen", "Pakarinen", "Kokko", "Virta", "Karjalainen", "Koivula", "Koivu", "Laukka", "Saari", "Kallio", "Hiltunen", "Rinne", "Rissanen", "Hirvonen", "Ranta", "Korpi", "Koskela", "Kukkonen"],
 
-kadunnimet=["Kapraantie","Testitie","Kuusitie","M\xe4ntykatu","Omenapolku","Vaahteratie","Katajakuja","Haavapolku","Tammikuja","Koivutie","Pihlajatie","Lehtikatu","Kastanjatie","Vadelmakuja","Herukkatie","Kirsikkatie","Mets\xe4tie","J\xe4rvenranta","Aurinkokatu","T\xe4htikuja","Kuunsilta","Purjeentie","Hiekkakuja","Merituuli","Vesitorninkatu","Satamakatu","Siltatie","Tulppaanikatu","Orvokkitie","Ruusupuisto","Lumikuja","Hangaspolku","Kimaltaj\xe4rvi","Kuutamotie","Revontulentie","T\xe4htisilm\xe4","Pilvitie","Sadekuja","Sateenkaarentie","Taikamets\xe4","Unikkopolku","Ruohokatu","Keltakuja","Sinipolku","Oranssikuja","Punatienniemi","Sinipiha","Vihersiili","Harmaaranta"],
+  kadunnimet = ["Kapraantie", "Testitie", "Kuusitie", "M\xe4ntykatu", "Omenapolku", "Vaahteratie", "Katajakuja", "Haavapolku", "Tammikuja", "Koivutie", "Pihlajatie", "Lehtikatu", "Kastanjatie", "Vadelmakuja", "Herukkatie", "Kirsikkatie", "Mets\xe4tie", "J\xe4rvenranta", "Aurinkokatu", "T\xe4htikuja", "Kuunsilta", "Purjeentie", "Hiekkakuja", "Merituuli", "Vesitorninkatu", "Satamakatu", "Siltatie", "Tulppaanikatu", "Orvokkitie", "Ruusupuisto", "Lumikuja", "Hangaspolku", "Kimaltaj\xe4rvi", "Kuutamotie", "Revontulentie", "T\xe4htisilm\xe4", "Pilvitie", "Sadekuja", "Sateenkaarentie", "Taikamets\xe4", "Unikkopolku", "Ruohokatu", "Keltakuja", "Sinipolku", "Oranssikuja", "Punatienniemi", "Sinipiha", "Vihersiili", "Harmaaranta"],
 
-kaupungit=["Lahti","Jyv\xe4skyl\xe4","Kuopio","Lappeenranta","Rovaniemi","Joensuu","Vaasa","Kajaani","Kokkola","Sein\xe4joki","Kouvola","H\xe4meenlinna","Mikkeli","Savonlinna","Porvoo","Varkaus","Hyvink\xe4\xe4","Salo","Kotka","Pietarsaari"],
-postinumerot=
-["15100","40100","70100","53100","96100","80100","65100","87100","67100","60100","45100","13100","50100","57100","06100","79100","78200","05800","24100","48100","68600"],
+  kaupungit = ["Lahti", "Jyv\xe4skyl\xe4", "Kuopio", "Lappeenranta", "Rovaniemi", "Joensuu", "Vaasa", "Kajaani", "Kokkola", "Sein\xe4joki", "Kouvola", "H\xe4meenlinna", "Mikkeli", "Savonlinna", "Porvoo", "Varkaus", "Hyvink\xe4\xe4", "Salo", "Kotka", "Pietarsaari"],
+  postinumerot =
+    ["15100", "40100", "70100", "53100", "96100", "80100", "65100", "87100", "67100", "60100", "45100", "13100", "50100", "57100", "06100", "79100", "78200", "05800", "24100", "48100", "68600"],
 
-PuhelinNumerot=["+358 404372343","+358 403843243","+358 405678901","+358 409876543","+358 406543210","+358 402345678","+358 407654321","+358 408765432","+358 401234567","+358 402345678","+358 409876543","+358 406543210","+358 405678901","+358 403843243","+358 407654321","+358 401234567","+358 408765432","+358 402345678","+358 403843243","+358 406543210","+358 405678901","+358 401234567","+358 408765432","+358 409876543","+358 407654321","+358 402345678","+358 405678901","+358 401234567","+358 408765432","+358 409876543","+358 402345678","+358 404372343","+358 405678901","+358 403843243","+358 401234567","+358 407654321","+358 406543210","+358 402345678"],
+  PuhelinNumerot = ["+358 404372343", "+358 403843243", "+358 405678901", "+358 409876543", "+358 406543210", "+358 402345678", "+358 407654321", "+358 408765432", "+358 401234567", "+358 402345678", "+358 409876543", "+358 406543210", "+358 405678901", "+358 403843243", "+358 407654321", "+358 401234567", "+358 408765432", "+358 402345678", "+358 403843243", "+358 406543210", "+358 405678901", "+358 401234567", "+358 408765432", "+358 409876543", "+358 407654321", "+358 402345678", "+358 405678901", "+358 401234567", "+358 408765432", "+358 409876543", "+358 402345678", "+358 404372343", "+358 405678901", "+358 403843243", "+358 401234567", "+358 407654321", "+358 406543210", "+358 402345678"],
 
-emailProviders=["gmail.com","protonmail.com","outlook.com","yahoo.com","icloud.com","yandex.com","zoho.com",],
+  emailProviders = ["gmail.com", "protonmail.com", "outlook.com", "yahoo.com", "icloud.com", "yandex.com", "zoho.com",],
 
-Sukupuoli=["Mies","Nainen"],
+  Sukupuoli = ["Mies", "Nainen"],
 
-Maksutapa=["Paypal","Skrill","Credit/Debit card","Bitcoin","Ethereum","Coinbase pay","Moon pay"],
+  Maksutapa = ["Paypal", "Skrill", "Credit/Debit card", "Bitcoin", "Ethereum", "Coinbase pay", "Moon pay"],
 
-Toimitustapa=["Posti automaatti","Ovelle","Postin toimipiste"],
+  Toimitustapa = ["Posti automaatti", "Ovelle", "Postin toimipiste"],
 
-products=[{Name:"Smartphone",Price:500,VAT:.24},{Name:"Laptop",Price:1e3,VAT:.24},{Name:"Tablet",Price:300,VAT:.24},{Name:"Headphones",Price:100,VAT:.24},{Name:"Smartwatch",Price:200,VAT:.24},{Name:"Camera",Price:600,VAT:.24},{Name:"Television",Price:800,VAT:.24},{Name:"Gaming Console",Price:400,VAT:.24},{Name:"Coffee Maker",Price:50,VAT:.24},{Name:"Blender",Price:60,VAT:.24},{Name:"Refrigerator",Price:700,VAT:.24},{Name:"Microwave",Price:80,VAT:.24},{Name:"Table Fan",Price:40,VAT:.24},{Name:"Air Purifier",Price:300,VAT:.24},{Name:"Wireless Mouse",Price:20,VAT:.24},{Name:"External Hard Drive",Price:120,VAT:.24},{Name:"Desk Chair",Price:150,VAT:.24},{Name:"LED Monitor",Price:250,VAT:.24},{Name:"Vacuum Cleaner",Price:90,VAT:.24},{Name:"Dishwasher",Price:400,VAT:.24},{Name:"Toaster Oven",Price:70,VAT:.24},{Name:"Smart Thermostat",Price:120,VAT:.24},{Name:"Digital Camera",Price:350,VAT:.24},{Name:"Fitness Tracker",Price:60,VAT:.24},{Name:"Electric Scooter",Price:250,VAT:.24},{Name:"Coffee Grinder",Price:30,VAT:.24},{Name:"Blu-ray Player",Price:80,VAT:.24},{Name:"Handheld Vacuum",Price:50,VAT:.24},{Name:"Wireless Earbuds",Price:100,VAT:.24},{Name:"Home Theater System",Price:300,VAT:.24},{Name:"Robot Vacuum",Price:220,VAT:.24},{Name:"Electric Kettle",Price:40,VAT:.24},{Name:"Digital Drawing Tablet",Price:150,VAT:.24},{Name:"Microwave Oven",Price:90,VAT:.24},{Name:"Bluetooth Speaker",Price:60,VAT:.24},{Name:"Hair Dryer",Price:25,VAT:.24},{Name:"Electric Toothbrush",Price:35,VAT:.24},{Name:"Bicycle",Price:300,VAT:.24},{Name:"Sofa",Price:800,VAT:.24},{Name:"Car Vacuum Cleaner",Price:50,VAT:.24},{Name:"Rice Cooker",Price:45,VAT:.24},{Name:"Massage Chair",Price:500,VAT:.24},{Name:"Washing Machine",Price:350,VAT:.24},]; 
+  products = [{ Name: "Smartphone", Price: 500, VAT: .24 }, { Name: "Laptop", Price: 1e3, VAT: .24 }, { Name: "Tablet", Price: 300, VAT: .24 }, { Name: "Headphones", Price: 100, VAT: .24 }, { Name: "Smartwatch", Price: 200, VAT: .24 }, { Name: "Camera", Price: 600, VAT: .24 }, { Name: "Television", Price: 800, VAT: .24 }, { Name: "Gaming Console", Price: 400, VAT: .24 }, { Name: "Coffee Maker", Price: 50, VAT: .24 }, { Name: "Blender", Price: 60, VAT: .24 }, { Name: "Refrigerator", Price: 700, VAT: .24 }, { Name: "Microwave", Price: 80, VAT: .24 }, { Name: "Table Fan", Price: 40, VAT: .24 }, { Name: "Air Purifier", Price: 300, VAT: .24 }, { Name: "Wireless Mouse", Price: 20, VAT: .24 }, { Name: "External Hard Drive", Price: 120, VAT: .24 }, { Name: "Desk Chair", Price: 150, VAT: .24 }, { Name: "LED Monitor", Price: 250, VAT: .24 }, { Name: "Vacuum Cleaner", Price: 90, VAT: .24 }, { Name: "Dishwasher", Price: 400, VAT: .24 }, { Name: "Toaster Oven", Price: 70, VAT: .24 }, { Name: "Smart Thermostat", Price: 120, VAT: .24 }, { Name: "Digital Camera", Price: 350, VAT: .24 }, { Name: "Fitness Tracker", Price: 60, VAT: .24 }, { Name: "Electric Scooter", Price: 250, VAT: .24 }, { Name: "Coffee Grinder", Price: 30, VAT: .24 }, { Name: "Blu-ray Player", Price: 80, VAT: .24 }, { Name: "Handheld Vacuum", Price: 50, VAT: .24 }, { Name: "Wireless Earbuds", Price: 100, VAT: .24 }, { Name: "Home Theater System", Price: 300, VAT: .24 }, { Name: "Robot Vacuum", Price: 220, VAT: .24 }, { Name: "Electric Kettle", Price: 40, VAT: .24 }, { Name: "Digital Drawing Tablet", Price: 150, VAT: .24 }, { Name: "Microwave Oven", Price: 90, VAT: .24 }, { Name: "Bluetooth Speaker", Price: 60, VAT: .24 }, { Name: "Hair Dryer", Price: 25, VAT: .24 }, { Name: "Electric Toothbrush", Price: 35, VAT: .24 }, { Name: "Bicycle", Price: 300, VAT: .24 }, { Name: "Sofa", Price: 800, VAT: .24 }, { Name: "Car Vacuum Cleaner", Price: 50, VAT: .24 }, { Name: "Rice Cooker", Price: 45, VAT: .24 }, { Name: "Massage Chair", Price: 500, VAT: .24 }, { Name: "Washing Machine", Price: 350, VAT: .24 },];
 
 const VAT = 0.24; // 24% VAT
 
@@ -52,7 +52,7 @@ function generateRandomPaymentConfirmation() {
 }
 
 function generateRandomShippingTracking() {
-  const carrierCompanies = ['Matkahuolto','Posti','DHL','UPS','DB   Schenker','Itella','PostNord','GLS','Schenker','FedEx'];
+  const carrierCompanies = ['Matkahuolto', 'Posti', 'DHL', 'UPS', 'DB   Schenker', 'Itella', 'PostNord', 'GLS', 'Schenker', 'FedEx'];
   const trackingNumber = Math.floor(Math.random() * 1000000000).toString();
   const estimatedDeliveryDate = new Date(new Date().getTime() + Math.random() * 30 * 24 * 60 * 60 * 1000);
   const carrierCompany = getRandomItemFromArray(carrierCompanies);
@@ -100,7 +100,7 @@ function generateRandomBuyOrder() {
   const transactionID = Math.floor(Math.random() * 1000000);
   const orderDate = new Date(
     new Date(2023, 0, 1).getTime() +
-      Math.random() * (new Date().getTime() - new Date(2023, 0, 1).getTime())
+    Math.random() * (new Date().getTime() - new Date(2023, 0, 1).getTime())
   );
 
   const paymentConfirmation = generateRandomPaymentConfirmation();
@@ -183,10 +183,13 @@ function generateAndPrintBuyOrderHTML(buyOrder, index) {
   htmlContent += '</ul></td>';
 
   htmlContent += `<td>${buyOrder.OrderTotal}</td>`;
-  htmlContent += `<td>${buyOrder.TransactionID}</td>`;
-  htmlContent += `<td>${buyOrder.OrderDate}</td>`;
-  htmlContent += `<td>${buyOrder.PaymentConfirmation.PaymentStatus}, Date: ${buyOrder.PaymentConfirmation.PaymentDate}</td>`;
-  htmlContent += `<td>${buyOrder.ShippingTracking.TrackingNumber}, Estimated Delivery Date: ${buyOrder.ShippingTracking.EstimatedDeliveryDate}, Carrier Company: ${buyOrder.ShippingTracking.CarrierCompany}</td>`;
+  htmlContent += `<td>Transaction ID: ${buyOrder.TransactionID}</td>`;
+  htmlContent += `<td>Order Date: ${buyOrder.OrderDate}</td>`;
+  htmlContent += `<td>${buyOrder.PaymentConfirmation.PaymentStatus}, 
+  Date: ${buyOrder.PaymentConfirmation.PaymentDate}</td>`;
+  htmlContent += `<td>Tracking number: <br>${buyOrder.ShippingTracking.TrackingNumber},<br><br>
+  Estimated Delivery Date:<br> ${buyOrder.ShippingTracking.EstimatedDeliveryDate},<br><br>
+  Carrier Company: <br>${buyOrder.ShippingTracking.CarrierCompany}</td><br>`;
   htmlContent += `<td>${buyOrder.OrderStatus}</td>`;
   htmlContent += `</tr>`;
 
@@ -287,7 +290,7 @@ rl.question('Enter the number of buy orders you want to generate: ', (numberOfBu
   } catch (err) {
     console.error('Error writing JSON file:', err);
   }
-  
+
   // Write the data to the CSV file
   csvWriter
     .writeRecords(randomBuyOrders)
